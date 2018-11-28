1035e681566a3a1f67e43c6296f0bffa522b26f9
wangrui <35493032@qq.com>
2018-11-28 15:32:48 +0800

    贪心算法 加工生产调度问题，和网上的解法不一样

--- greedy.py
+++ greedy.py
@@ -13,6 +13,8 @@ def broadcast():
     贪婪算法作为近似算法，步骤如下：
     1. 选出这样一个广播台，即它覆盖了最多的未覆盖州。即便这个广播台覆盖了一些已覆盖的州，也没有关系。
     2. 重复第一步，直到覆盖了所有的州。
+
+    覆盖最多优先策略
     """
     states_needed = set(['mt', 'wa', 'or', 'id', 'nv', 'ut', 'ca', 'az'])
     final_stations = set()
@@ -36,6 +38,7 @@ def broadcast():
 def dispatcher():
     """
     区间调度问题
+    结束时间优先的策略
     :return:
     """
     jobs = [(9, 10), (11, 12), (10, 11), (9.5, 10.5), (10.5, 11.5)]
@@ -49,6 +52,34 @@ def dispatcher():
     print(ret)
 
 
+def job_dispatcher():
+    """
+https://blog.csdn.net/niuniu0205/article/details/81449538
+某工厂收到了n个产品的订单,这n个产品分别在A、B两个车间加工,并且必须先在A车间加工后才可以到B车间加工。
+某个产品i在A、B两车间加工的时间分别为Ai、Bi。怎样安排这n个产品的加工顺序,才能使总的加工时间最短。
+这里所说的加工时间是指：从开始加工第一个产品到最后所有的产品都已在A、B两车间加工完毕的时间。
+Input
+第一行仅—个数据n(0 < n < 1000),表示产品的数量。接下来n个数据是表示这n个产品在A车间加工各自所要的时间(都是整数)。
+最后的n个数据是表示这n个产品在B车间加工各自所要的时间(都是整数)。
+Output
+第一行一个数据,表示最少的加工时间;第二行是一种最小加工时间的加工顺序。
+Sample Input
+5 3 5 8 7 10 6 2 1 4 9
+Sample Output
+34 1 5 4 2 3
+    :return:
+
+    A-B最小优先策略,?????不知道对不对，和网上的不一样
+    """
+    n = 5
+    jobs = [(3,6),(5,2),(8,1),(7,4),(10,9)]
+    newjobs = sorted(jobs,key=lambda x:x[0]-x[1])
+    ret = sum([job[0] for job in newjobs])+newjobs[-1][1]
+    print([jobs.index(nj)+1 for nj in newjobs])
+    print(ret)
+
+
 if __name__ == '__main__':
     # broadcast()
-    dispatcher()
+    # dispatcher()
+    job_dispatcher()
